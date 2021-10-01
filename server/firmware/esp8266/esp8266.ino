6ac91201e76319c32b293dd3c9d58ba1745a010b
rodriguesfas <franciscosouzaacer@gmail.com>
2021-10-01 15:42:56 -0300

    add new dashboard

--- server/firmware/esp8266/esp8266.ino
+++ server/firmware/esp8266/esp8266.ino
@@ -7,53 +7,67 @@ José Cintra em Outubro de 2019 - www.josecintra.com/blog/comunicacao-websockets
 #include <WebSocketsServer.h>
 #include <neotimer.h>
 
-//Correção nivel lógico invertido
+// Correção nivel lógico invertido
 #define ON LOW
 #define OFF HIGH
 
+unsigned char address = 0;
+
 WebSocketsServer webSocket = WebSocketsServer(81); // Recebe dados do cliente
-Neotimer mytimer = Neotimer(10000); // Intervalo de 10 segundos para envio dos dados do sensor
+Neotimer mytimer = Neotimer(10000);                // Intervalo de 10 segundos para envio dos dados do sensor.
 
 // Autenticação wi-fi - Coloque aqui a sua configuração
-const char* ssid     = "brisa-679421";
-const char* password = "84bmey4i";
+const char *ssid = "brisa-679421";
+const char *password = "84bmey4i";
 
 const int pinLED = D4;
 String tempString; // Temperatura convertida para String
 
-// Tratamento de eventos dos dados que vêm do cliente 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
+/**
+ *  webSocketEvent - Tratamento de eventos dos dados que vêm do cliente.
+ */
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
+{
 
-  switch (type) {
-    case WStype_DISCONNECTED:
-      break;
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    break;
 
-    case WStype_CONNECTED:
-      { IPAddress ip = webSocket.remoteIP(num);
-        Serial.println(ip);
-      }
-      break;
-
-    case WStype_TEXT:
-      { String text = String((char *) &payload[0]);
-        Serial.println(text);
-        Serial.println(num);
-        Serial.println(type);
-
-        if (text == "ON") {
-          digitalWrite(pinLED, ON);
-        } else {
-          digitalWrite(pinLED, OFF);
-        }
-      }
-      break;
-
+  case WStype_CONNECTED:
+  {
+    IPAddress ip = webSocket.remoteIP(num);
+    address = num;
+    Serial.println(ip);
   }
+  break;
 
+  case WStype_TEXT:
+  {
+    String text = String((char *)&payload[0]);
+    Serial.println(text);
+    Serial.println(num);
+    Serial.println(type);
+
+    if (text == "ON")
+    {
+      digitalWrite(pinLED, ON);
+    }
+    else
+    {
+      digitalWrite(pinLED, OFF);
+    }
+  }
+  break;
+  }
 }
 
-void setup() {
-  
+/**
+ * setup
+ */
+void setup()
+{
+
   // Inicialização do LED
   Serial.begin(9600);
   pinMode(pinLED, OUTPUT);
@@ -61,24 +75,29 @@ void setup() {
 
   // Conexões wi-fi e websocket
   WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(". ");
     delay(100);
   }
-  
+
   Serial.println(WiFi.localIP());
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
-
 }
 
-void loop() {
+/**
+ * loop
+ */
+void loop()
+{
   webSocket.loop();
 
   // Envio periódico dos dados do sensor de temperatura para o cliente
-  if (mytimer.repeat()) {;
-    webSocket.sendTXT(0, "32");
+  if (mytimer.repeat())
+  {
+    ;
+    webSocket.sendTXT(address, "32");
   }
 }
-
